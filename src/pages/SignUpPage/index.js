9daba7dd749dc953550c214b5d31583427f06468
SeungMin <osminosmin7180@gmail.com>
2023-06-08 23:05:11 +0900

    modify signup page

--- src/pages/SignUpPage/index.js
+++ src/pages/SignUpPage/index.js
@@ -5,16 +5,31 @@ import axios from 'axios';
 import './SignUpPage.css';
  
 function LoginPage() {
+    const [inputName, setInputName] = useState('')
+    const [inputPhoneNumber, setInputphoneNumber] = useState('')
     const [inputId, setInputId] = useState('')
     const [inputPw, setInputPw] = useState('')
- 
+    const [inputPwCheck, setInputPwCheck] = useState('')
+
 	// input data 의 변화가 있을 때마다 value 값을 변경해서 useState 해준다
+    const handleInputName = (e) => {
+        setInputName(e.target.value)
+    }
+
+    const handleInputPhoneNumber = (e) => {
+        setInputphoneNumber(e.target.value)
+    }
+
     const handleInputId = (e) => {
         setInputId(e.target.value)
     }
  
     const handleInputPw = (e) => {
         setInputPw(e.target.value)
+    }
+
+    const handleInputPwCheck = (e) => {
+        setInputPwCheck(e.target.value)
     }
  
 	// login 버튼 클릭 이벤트
@@ -33,19 +48,49 @@ function LoginPage() {
  
     return(
       <div className='signup-page'>
+        <a href="/" className="logo">
+          Traveling
+        </a>
+
+        {/* 회원가입 탭 메뉴 만들기 */}
+
         <div className='signin'>
-            <h2>Sign In</h2>
+            <h2>Sign Up</h2>
+            <div class="jb-division-line"></div>
+
+            <div className='text-filed'>
+                <label htmlFor='input_pw_check'>이름 : </label>
+                <input type='text' name='input_name' value={inputName} onChange={handleInputName} />
+            </div>
+            <div className='text-filed'>
+                <label htmlFor='input_phone_number'>전화번호 : </label>
+                <input type='number' name='input_phoneNumber' value={inputPhoneNumber} onChange={handleInputPhoneNumber} />
+            </div>
             <div className='text-filed'>
                 <label htmlFor='input_id'>ID : </label>
                 <input type='text' name='input_id' value={inputId} onChange={handleInputId} />
+                <div className='errorMessage_ID'>
+                    중복된 아이디 입니다.
+                </div>
             </div>
-            <div className='text-filed2'>
+            <div className='text-filed'>
                 <label htmlFor='input_pw'>PW : </label>
                 <input type='password' name='input_pw' value={inputPw} onChange={handleInputPw} />
             </div>
-              <button className='submit-btn' type='button' onClick={onClickLogin}>Login</button>
+
+            <div className='text-filed'>
+                <label htmlFor='input_pw_check'>PW Check : </label>
+                <input type='password' name='input_pw_check' value={inputPwCheck} onChange={handleInputPwCheck} />
+                <div className='errorMessage_PW'>
+                    비밀번호가 일치하지 않습니다.
+                </div>
+            </div>
+
+
+
+              <button className='submit-btn' type='button' onClick={onClickLogin}>다음</button>
             <div classname = "link_password">
-              <a href='/'>비밀번호를 잊어버리셨나요?</a>
+              {/* <a href='/'>비밀번호를 잊어버리셨나요?</a> */}
             </div>
         </div>
       </div>
